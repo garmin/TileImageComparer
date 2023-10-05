8e5f30dc0a14f2906c1e01ee246b4ff4c6f3df5c
fmatteoni-nav <federico.matteoni@garmin.com>
2023-10-05 10:49:38 +0200

    Dataset

--- benchmark.sh
+++ benchmark.sh
@@ -34,7 +34,7 @@ for subdir in $subdirs; do
             fi
         done <<< "$image_files"
         
-        output=$(./ComparerPlayground -f1 $image1 -f2 $image2 -s 1024 -b --algorithm line)
+        output=$(./ComparerPlayground -f1 $image1 -f2 $image2 -s 1024 -b --algorithm custom)
         if [ $? -eq 0 ]; then
             correct=$(expr $correct + 1)
         fi
